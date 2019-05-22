3d71fbf697751d0b317b5ebe25639f07afc32006
ovidiu-dancila <ovidiu.dancila@gmail.com>
2019-05-22 23:46:19 +0300

    Finished

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ClientThread.java
@@ -0,0 +1,71 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread extends Thread {
+    private String address;
+    private int port;
+    private String city;
+    private TextView infoTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String city, TextView infoTextView) {
+        this.address = address;
+        this.port = port;
+        this.city = city;
+        this.infoTextView = infoTextView;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            printWriter.println(city);
+            printWriter.flush();
+
+            String information;
+            while ((information = bufferedReader.readLine()) != null) {
+                final String finalizedInformation = information;
+                infoTextView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        infoTextView.setText(finalizedInformation);
+                    }
+                });
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,112 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    // Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText cityEditText = null;
+    private Button getButton = null;
+    private TextView infoTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Server port should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() == null) {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not create server thread!");
+                return;
+            }
+
+            serverThread.start();
+        }
+
+    }
+
+    private GetButtonClickListener getButtonClickListener = new GetButtonClickListener();
+    private class GetButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String clientAddress = clientAddressEditText.getText().toString();
+            String clientPort = clientPortEditText.getText().toString();
+            if (clientAddress == null || clientAddress.isEmpty()
+                    || clientPort == null || clientPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Client connection parameters should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (serverThread == null || !serverThread.isAlive()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] There is no server to connect to!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String city = cityEditText.getText().toString();
+            if (city == null || city.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] City parameter should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            infoTextView.setText(Constants.EMPTY_STRING);
+
+            clientThread = new ClientThread(
+                    clientAddress, Integer.parseInt(clientPort), city, infoTextView
+            );
+            clientThread.start();
+        }
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+
+        serverPortEditText = (EditText) findViewById(R.id.server_port_edit_text);
+        connectButton = (Button) findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientAddressEditText = (EditText) findViewById(R.id.server_address_edit_text);
+        clientPortEditText = (EditText) findViewById(R.id.server_port_from_client);
+        cityEditText = (EditText) findViewById(R.id.city_edit_text);
+        getButton = (Button) findViewById(R.id.get_button);
+        getButton.setOnClickListener(getButtonClickListener);
+        infoTextView = (TextView) findViewById(R.id.info_text_view);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onDestroy() callback method has been invoked");
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
+    }
+
+}
